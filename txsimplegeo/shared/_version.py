4ae9c174aed6ae0da3fc540f5633b12572adda57
Zooko Ofsimplegeo <zooko@simplegeo.com>
2011-01-20 10:09:20 -0700

    version file formatted so hudson can update the build-number stamp without conflicting with the manually-controlled version-number stamp

--- txsimplegeo/shared/_version.py
+++ txsimplegeo/shared/_version.py
@@ -1,6 +1,14 @@
 # This is the version of this source code.
 
-verstr = "0.9"
+manual_verstr = "0.9"
+
+
+
+auto_build_num = "0"
+
+
+
+verstr = manual_verstr + "." + auto_build_num
 try:
     from pyutil.version_class import Version as pyutil_Version
     __version__ = pyutil_Version(verstr)
