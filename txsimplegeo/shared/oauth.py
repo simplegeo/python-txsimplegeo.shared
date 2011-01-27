8e0d75f4b43604acb7cdc6208ef26512e015aee5
Zooko Ofsimplegeo <zooko@simplegeo.com>
2011-01-27 15:46:56 -0700

    fix missing import by copying in function to_unicode

--- txsimplegeo/shared/oauth.py
+++ txsimplegeo/shared/oauth.py
@@ -1,9 +1,19 @@
 # copied from https://github.com/simplegeo/python-oauth2 and modified
 # to do only what we need here
 
-from simplegeo.shared import to_unicode
-
 import hashlib, hmac, urllib
+
+def to_unicode(s):
+    """ Convert to unicode, raise exception with instructive error
+    message if s is not unicode, ascii, or utf-8. """
+    if not isinstance(s, unicode):
+        if not isinstance(s, str):
+            raise TypeError('You are required to pass either unicode or string here, not: %r (%s)' % (type(s), s))
+        try:
+            s = s.decode('utf-8')
+        except UnicodeDecodeError, le:
+            raise TypeError('You are required to pass either a unicode object or a utf-8 string here. You passed a Python string object which contained non-utf-8: %r. The UnicodeDecodeError that resulted from attempting to interpret it as utf-8 was: %s' % (s, le,))
+    return s
 
 def escape(s):
     """Escape a URL including any /."""
